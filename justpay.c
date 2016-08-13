99b70b6679f2582e645110bf35d38731607690f4
Rubala <rubala333@gmail.com>
2016-08-13 13:09:49 +0530

    Update justpay.c

--- justpay.c
+++ justpay.c
@@ -4,14 +4,11 @@ int main()
 {
 int n;
 scanf("%d",&n);
-if(n<0)
-{
 if(n==0)
 printf("%d is zero",n);
-else
+else if(n<0)
 printf("%d is negative",n);
-}
-else
+else if(n>0)
 printf("%d is positive",n);
 return 0;
 }
